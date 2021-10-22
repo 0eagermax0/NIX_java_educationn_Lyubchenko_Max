8604459f238d4a2bd68bc8bb9f1205ee39a78432
0eagermax0 <m.liubchenko@student.csn.khai.edu>
2021-10-23 01:08:39 +0300

    Coffee Machine Project - Stage 3

--- src/nix/education/java/coffeemachine/CoffeeMachine.java
+++ src/nix/education/java/coffeemachine/CoffeeMachine.java
@@ -16,5 +16,39 @@ public class CoffeeMachine {
         int numMilk = numCup * 50;
         int numCoffee = numCup * 15;
         System.out.println(numWater + " ml of water\n" + numMilk + " ml of milk\n" + numCoffee + " g of coffee beans");
+        System.out.println("Write how many ml of water the coffee machine has:");
+        System.out.print("> ");
+        numWater = scan.nextInt();
+        System.out.println("Write how many ml of milk the coffee machine has:");
+        System.out.print("> ");
+        numMilk = scan.nextInt();
+        System.out.println("Write how many grams of coffee beans the coffee machine has:");
+        System.out.print("> ");
+        numCoffee = scan.nextInt();
+        do {
+            System.out.println("Write how many cups of coffee you will need:");
+            System.out.print("> ");
+            numCup = scan.nextInt();
+            if (numCup == 0)
+                System.out.println("Fail! Try again.");
+        } while (numCup == 0);
+        numWater = numWater / 200;
+        numMilk = numMilk / 50;
+        numCoffee = numCoffee / 15;
+        int min;
+        if (numWater >= numMilk)
+            min = numMilk;
+        else
+            min = numWater;
+        if (min >= numCoffee)
+            min = numCoffee;
+        if (min == numCup)
+            System.out.println("Yes, I can make that amount of coffee");
+        else if(min > numCup) {
+            int N = min - numCup;
+            System.out.println("Yes, I can make that amount of coffee (and even " + N + " more than that)");
+        }
+        else
+            System.out.println("No, I can make only " + min + " cups of coffee");
     }
 }
